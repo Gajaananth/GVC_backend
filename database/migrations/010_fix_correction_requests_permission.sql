c78edbbebcb06f9bb8aa1163290e549e5006d9e8
Gajaananth <gajaananthnadan17898@gmail.com>
2026-06-06 21:31:29 +0530

    Clean up migration file and add schema usage grant for service_role

--- database/migrations/010_fix_correction_requests_permission.sql
+++ database/migrations/010_fix_correction_requests_permission.sql
@@ -1,7 +1,8 @@
--- Migration 010: Grant service_role access to collection correction requests
+﻿-- Migration 010: Grant service_role access to collection correction requests
 
 BEGIN;
 
+GRANT USAGE ON SCHEMA public TO service_role;
 GRANT SELECT, INSERT, UPDATE, DELETE ON public.collection_correction_requests TO service_role;
 
 COMMIT;
